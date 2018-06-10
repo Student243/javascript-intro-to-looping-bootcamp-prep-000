1ce164b91dff8f499ef93136c5eaeddba7857a7c
Kevin Andrew Johnson <kjohnson5315@hotmail.com>
2018-06-10 23:57:46 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array) {
+  array = [...array];
+for (let i = 0; i < 25; i++) {
+  array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+ }
+ return array
+}
+function whileLoop(n) {
+  let countdown = 100;
+  while (n > 0) {
+    console.log (--n)
+  }
+  return `done`
+}
+function maybeTrue(array) {
+  return array.length > 0 && maybeTrue()
+}
+  do {
+  console.log(element)
+  } while (doWhileLoop());
